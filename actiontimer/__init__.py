109328057b40f223d738690f4e1ab7790d23d852
Johannes Eckstein <digital.fireball@gmail.com>
2016-07-03 14:12:15 +0200

    Update __init__.py
    removed some comments

--- actiontimer/__init__.py
+++ actiontimer/__init__.py
@@ -8,28 +8,18 @@ except NameError:
 
     __file__ = sys.argv[0]
 
-    print("Running:", __file__,"...")
+    #print("Running:", __file__,"...")
 
 __root__ = os.path.dirname(os.path.realpath(__file__))
 
-#if os.path.exists(os.path.join(__root__,"library.zip")):
-#    __root__ = os.path.join(__root__,"library.zip")
-#    _basePath = os.path.dirname(__file__)
-#
-#    #print ("at location:", __root__)
-#    __examples__ = None
-#else:
-#    __examples__ = os.path.realpath(os.path.join(__root__,"example"))
-
 # Python3
 if os.name == 'nt':
-    # We are currently only developing for Windows-Clients
+    # Windows
     # Checking if environment is set-up correctly
-
     #if not os.path.isdir(__root__): # check for dev-environment
-    #    __root__ = 'C:/Znake-Tool/'  # else set release-env
     sys.path.append(__root__)
 else:
+    # Others
     sys.path.append(__root__)
 
 
